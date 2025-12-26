b884af039ee130d378bfcdf63b2e1a19dbdad26c
immanuvelponrani <immanuvel7598@gmail.com>
2025-12-26 16:13:26 +0530

    Condition Demo

---
+++ DemoCondition.java
@@ -0,0 +1,16 @@
+class DemoCondition
+{
+	public static void main(String[] args)
+	{ 
+		int a = 10;
+		
+		if(a>=18)
+		{
+			System.out.println("Voting Ready");
+		}
+		else
+		{	
+			System.out.println("Not Eligible");
+		}
+	}
+}
