415ae8df3382ab0f9bbd1a235772f7f9f3c065cc
immanuvelponrani <immanuvel7598@gmail.com>
2026-01-13 13:59:19 +0530

    Add GameRankCorrect class to calculate rank

---
+++ j53/GameRankCorrect.java
@@ -0,0 +1,11 @@
+class GameRankCorrect
+{
+    public static void main(String args[])
+    {
+        long score = 500;
+        double time = 3.5;
+
+        double rank = (double) score / time;
+        System.out.println(rank);
+    }
+}
