cf35d08ca19ef983dc6cb895bba09a56451f77f7
immanuvelponrani <immanuvel7598@gmail.com>
2026-03-31 10:40:36 +0530

    Create Student table with initial data

---
+++ DB-Datatypes/student.sql
@@ -0,0 +1,8 @@
+CREATE TABLE Student (
+    id INT,
+    name VARCHAR(50),
+    age INT,
+    dob DATE
+);
+INSERT INTO Student VALUES (1, 'Arun', 20, '2005-06-10');
+INSERT INTO Student VALUES (2, 'Priya', 22, '2003-02-15');
